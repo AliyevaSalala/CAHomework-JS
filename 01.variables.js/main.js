78b86c73038e634bcd098294c3cbaa3729f16276
AliyevaSalala <aselale58@gmail.com>
2023-10-30 20:59:56 +0300

    js

---
+++ 01.variables.js/main.js
@@ -0,0 +1,25 @@
+let fisrtName = "Shalala";
+let lastName = "Aliyeva";
+let score = 21;
+console.log(fisrtName, lastName, score);
+console.log("firstname:" + fisrtName + "lastName" + lastName);
+
+let universtyName = " GDU";
+universtyName = "ADAU";
+console.log(universtyName);
+
+let fisrtNum = 5;
+let secondNum = 7;
+sum = fisrtNum + secondNum;
+multipiy = fisrtNum * secondNum;
+subtruct = fisrtNum - secondNum;
+division = fisrtNum / secondNum;
+reaminder = fisrtNum % secondNum;
+power = fisrtNum ** secondNum;
+
+console.log("sum", sum);
+console.log("multioiu", multipiy);
+console.log("subtruct", subtruct);
+console.log("division", division);
+console.log("reaminder", reaminder);
+console.log("power", power)
