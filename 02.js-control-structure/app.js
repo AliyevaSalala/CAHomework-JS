32dd1644d3177e43b0ea932213b222ab61f26877
AliyevaSalala <aselale58@gmail.com>
2023-11-01 05:48:18 +0300

    js

---
+++ 02.js-control-structure/app.js
@@ -0,0 +1,13 @@
+let katet1 = 3;
+let katet2 = 4;
+let hipotenuz = 5;
+
+//let katet1=3;
+//let katet2=3;
+//let hipotenuz=8;
+
+if (katet1 ** 2 + katet2 ** 2 == hipotenuz ** 2) {
+  console.log(`Duzbucaqli ucbucaqdir..`);
+} else {
+  console.log(`Duzbucaqli ucbucaq degil..`);
+}
