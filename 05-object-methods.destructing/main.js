616281daa0f296298b03ada548c495d3d2d89a85
AliyevaSalala <aselale58@gmail.com>
2023-11-15 23:06:41 -0800

    js

--- 05-object-methods.destructing/main.js
+++ 05-object-methods.destructing/main.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 2, 2, 4, 3, 3, 2, 4, 7, 5, 7, 6,];
+let arr = [1, 2, 2, 2, 4, 3, 3, 2, 4, 7, 5, 7, 6];
 
 //1. remove dublicated values
 
@@ -12,7 +12,7 @@ console.log(removeDuplicates(arr));
 function findCountDigitOfArray(array, number) {
   let count = 0;
   arr.forEach((num) => num === number && count++);
-  return count;
+  return ` ${number} ededi ${count} defe tekrarlanir `;
 }
 
 console.log(findCountDigitOfArray(arr, 7));
@@ -28,18 +28,19 @@ function generateOtp() {
   }
   return OTP;
 }
-  console.log(generateOtp());
+console.log(generateOtp());
 
 //-----------4. find all truth values in myArr
 
 let myArr = [1, 4, true, false, 0, 5, "hello", NaN, undefined];
 
-let allTrue = myArr.filter(Boolean);
-console.log(allTrue);
-
+// let allTrue = myArr.filter(Boolean);
+// console.log(allTrue);
+console.log(myArr.filter((item) => Boolean(item)));   // All trues values
+// console.log(myArr.filter((item) => !Boolean(item)));   // All false values
 //5. Arraydaki en boyuk reqemi tapan ve nece defe tekrarlandigini gosteren funksiya yazın.
 
-let max=Math.max(...arr)
+let max = Math.max(...arr);
 function findMaxDigitOfArray(arr, number) {
   let count = 0;
   arr.forEach((num) => num === max && count++);
@@ -48,4 +49,3 @@ function findMaxDigitOfArray(arr, number) {
 
 console.log(findMaxDigitOfArray(arr));
 // console.log(` ${Math.max(...arr)}  ${findMaxDigitOfArray(arr, 7)}`);
-
